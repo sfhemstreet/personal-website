885af0a96f3bf1f45d2cf3596f48f41e58512466
sfhemstreet <sfhemstreet@gmail.com>
2020-06-24 18:28:28 -0700

    Fix logo SVG

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -46,15 +46,15 @@ export const mockEcommerce: Project = {
     "PostgreSQL",
   ],
 
-  difficulties: `Managing state and persistant storage. 
+  difficulties: `Managing state and persistent storage. 
                 When navigating between pages state is lost, such as number of items in the shopping cart. 
                 A page needs to have the previous state to display components properly,
                 failing to do so can create unwanted effects such as flickering.  
                 <br>
                 I initially planned to use Redux to manage state, and use storage (localStorage/sessionStorage) 
-                to save it, so that when a new page is loaded, the previous state in storage is used to initalize the Redux store. 
+                to save it, so that when a new page is loaded, the previous state in storage is used to initialize the Redux store. 
                 It was very complicated creating the store from storage because the way Next.js 
-                handles routing on a staic site is a mix bewteen client side  
+                handles routing on a static site is a mix between client side  
                 Components connected to the store would often render with stale data, and it generally felt wrong.`,
 
   solution: `I researched and found the SWR package, which is a set of React hooks meant for remote data fetching, that 
@@ -75,7 +75,7 @@ export const mockEcommerce: Project = {
   features: [
     "Decoupled frontend architecture",
     "Responsive layout",
-    "Persistant state",
+    "Persistent state",
     "Static page generation",
   ],
 
@@ -105,7 +105,7 @@ export const mapAndWeather: Project = {
                 Check it out on GitHub, you can download, install and run it locally.`,
   solution: `Wrote a simple reusable component for rendering a leaflet map.`,
   features: [
-    "Resposive layout",
+    "Responsive layout",
     "Multiple map settings",
     "Weather imaging",
     "Search and filter list of points of interest",
@@ -115,7 +115,7 @@ export const mapAndWeather: Project = {
 
 export const traderBot: Project = {
   name: "Trader Bot",
-  description: `Python program to help automate cryptocurrency trades. 
+  description: `Python program to help automate cryptocurrency trading. 
                 It uses real-time bitcoin inflow/outflow data from Token Analyst's 
                 websocket feed to trigger trades on the BitMEX exchange. 
                 Connecting to both the Token Analyst and BitMEX websockets, 
@@ -134,8 +134,8 @@ export const traderBot: Project = {
 
   solution: `I went with an single threaded event loop approach because it seemed the simplest.  At a high level the 
             program waits for data, and reacts when it arrives, so I used asyncio to create an event loop, 
-             gave it tasks that yeild data, and told it to run forver. When a 
-             task yeilds data from a websocket it calls a function that starts the trading process.`,
+             gave it tasks that yield data, and told it to run forever. When a 
+             task yields data from a websocket it calls a function that starts the trading process.`,
 
   features: ["Simple API", "OOP architecture", "Websockets", "asyncio"],
   githubLink: "https://github.com/sfhemstreet/TraderBot",
@@ -169,7 +169,7 @@ export const choreApp: Project = {
     "Express",
     "Json Web Tokens",
     "Knex",
-    "Bcyrpt",
+    "Bcrypt",
     "SendGrid",
   ],
 
@@ -177,7 +177,12 @@ export const choreApp: Project = {
                 I researched a lot before going with a session-cookie approach to track user sessions. 
                 It worked well till the first deployment and testing on a real mobile device.`,
   solution: `I quickly changed to a Json Web Token approach, which worked well.`,
-  features: ["Single Page Application", "User authenicatoin", "Json Web Token"],
+  features: [
+    "Single Page Application",
+    "User authentication",
+    "Json Web Token",
+    "Responsive layout",
+  ],
   githubLink: "https://github.com/sfhemstreet/chore-app",
   liveLink: "https://chore-app-frontend.herokuapp.com/",
 };
@@ -230,7 +235,7 @@ export const rockPaperScissors: Project = {
   ],
   technologies: ["React", "Next.js", "TypeScript", "Styled-Components"],
   difficulties: `Animating pieces between game play states.`,
-  solution: `Keep app layout simple, don't rerender game pieces once on screen.`,
+  solution: `Keep app layout simple, don't re-render game pieces once on screen.`,
   features: ["Clean UI", "Followed design to the pixel"],
   githubLink: "https://github.com/sfhemstreet/rock_paper_scissors",
   liveLink: "https://rock-paper-scissors-six.now.sh/",
@@ -242,15 +247,15 @@ export const alienInvasion: Project = {
   images: [
     {
       src: images["alien-invasion-small"],
-      alt: "Screen shot of Alien Invasion gameplay",
+      alt: "Screen shot of Alien Invasion game play",
     },
   ],
   technologies: ["HTML5", "Canvas", "JavaScript"],
   difficulties: "Making the game hard, but not impossible.",
   solution: `Tweak level difficultly after testing. 
             Add easter egg magic blue box that replaces your ship's 
-            rockets with a proton-7 laser temporaraly.`,
-  features: ["Old school hard gameplay", "60fps", "Incredible graphics"],
+            rockets with a proton-7 laser temporarily.`,
+  features: ["Old school hard game play", "60fps", "Incredible graphics"],
   githubLink: "https://github.com/sfhemstreet/alien-invasion-js",
   liveLink: "https://sfhemstreet.github.io/alien-invasion-js/",
 };
